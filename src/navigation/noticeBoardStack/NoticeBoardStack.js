901da84846755350a7506954c855cbfec1ec7e01
Abdullah Malik <abdullah.malik@doctornow.io>
2025-05-12 17:16:29 +0500

    fix: normalize folder casing for noticeboard screen

--- src/navigation/noticeBoardStack/NoticeBoardStack.js
+++ src/navigation/noticeBoardStack/NoticeBoardStack.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import NoticeBoardScreen from '../../screens/noticeBoardScreen/NoticeBoardScreen';
+import NoticeBoardScreen from '../../screens/noticeboardScreen/NoticeBoardScreen';
 import CreatePostScreen from '../../screens/createPostScreen/CreatePostScreen';
 import ColorPalette from '../../constants/ColorPalette';
 
